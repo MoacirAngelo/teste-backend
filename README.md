68672be1df8777100308f153f4c48d462e17b060
MoacirAngelo <moacirangelos@gmail.com>
2022-10-26 10:12:26 -0300

    feat: adiciona READE.ME e env_file

---
+++ README.md
@@ -0,0 +1,17 @@
+# Teste Backend 
+Projeto para o teste para vaga de backend na M10web.
+
+## 🚀 Começando
+
+Essas instruções permitirão que você obtenha uma cópia do projeto em operação na sua máquina local para fins de desenvolvimento e teste.
+
+
+## 🖥️ Como rodar este projeto?
+    Para iniciar essa aplicação localmente, é preciso baixar este reepositório e modificar o arquivo 
+chamado de *env_file*.Esse arquivo precisa ser renomeado para *.env* e os valores das variaveis devem ser configuradas 
+de acordo com a sua conexão de banco de dados.
+
+-Crie o Banco de dados MySQL usando phpmyadmin ou outro Gerenciador de banco de dados.
+
+- Rode os comandos npm i para instalar as dependências e npm start para iniciar o projeto.
+
--- index.js
+++ index.js
@@ -9,11 +9,13 @@ const { existsOrError, notExistsOrError, equalsOrError } = require('./validation
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+require('dotenv').config()
+
 const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'username',
-  password : '123',
-  database : 'db_teste_backend'
+  user     :  process.env.DB_USERNAME,
+  password :  process.env.DB_PASSWORD,
+  database :  process.env.DB_NAME
 })
 
 connection.connect((err) => {
@@ -42,7 +44,6 @@ app.get("/usuarios",(req,res) => {
     })
 })
 
-// Retrieve user with id 
 app.get('/usuarios/:id', function (req, res) {
     let user_id = req.params.id;
     if (!user_id) {
@@ -179,11 +180,6 @@ const query  = `UPDATE usuarios SET nome='${user.nome}', sobrenome='${user.sobre
 
 //ENDEREÇOS
 //1.1 Método [GET]
-/*
-- URL {{url-base}}/enderecos-usuario/:id_usuario
-- Função: Listar todos os endereços de acordo com o usuário especificado via
-url: id_usuario
-*/
 
 app.get("/enderecos-usuario/:id_usuario",(req,res) => {
     
@@ -211,10 +207,7 @@ app.get("/enderecos-usuario/:id_usuario",(req,res) => {
 
 /*
 1.2 Método [GET]
-- URL {{url-base}}/enderecos-usuario/:id_endereco_usuario
-- Função: Listar endereço especificado via url: id_endereco_usuario
 */
-
 app.get("/enderecos-usuario/:id_endereco_usuario",(req,res) => {
     
     const query = 'SELECT * FROM enderecos_usuario WHERE id_endereco_usuario=' + parseInt(req.params.id_endereco_usuario)
@@ -238,12 +231,7 @@ app.get("/enderecos-usuario/:id_endereco_usuario",(req,res) => {
     })
 })
 
-
-
-
 //1.3 Método [POST]
-//- URL {{url-base}}/enderecos-usuario
-
 app.post("/enderecos-usuario",(req,res) => {
     let endereco = req.body
     const values = [
@@ -294,10 +282,7 @@ app.post("/enderecos-usuario",(req,res) => {
 
 /*
 1.4 Método [DELETE]
-- URL {{url-base}}/enderecos-usuario/:id_endereco_usuario
-- Função: Remover endereço especificado via url: id_endereco_usuario
 */
-
 app.delete('/enderecos-usuario/:id_endereco_usuario', function (req, res) {
     const id_endereco_usuario = parseInt(req.params.id_endereco_usuario)
 
@@ -327,8 +312,6 @@ app.delete('/enderecos-usuario/:id_endereco_usuario', function (req, res) {
 
 /*
 1.1 Método [PUT]
-- URL {{url-base}}/enderecos-usuario/:id_endereco_usuario
-- Função: Editar endereço especificado via url: id_enderec
 */
 app.put('/enderecos-usuario/:id_endereco_usuario', function (req, res) {
 
@@ -360,7 +343,6 @@ app.put('/enderecos-usuario/:id_endereco_usuario', function (req, res) {
     if(endereco.complemento) query += `complemento = '${endereco.complemento}' `
 
     connection.query(query, (error, results, fields) => {
-    //if (error) throw error;
         if (error){
             const msg = {
                 "codigo": 400,
